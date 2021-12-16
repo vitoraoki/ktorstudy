95a0ba0447483b80db8bc517b1591c64dc1cff58
Vitor <vitor-27@hotmail.com>
2021-12-16 01:56:03 -0300

    Update Customer request object to not require id

--- src/main/kotlin/com/example/model/Customer.kt
+++ src/main/kotlin/com/example/model/Customer.kt
@@ -4,7 +4,7 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class Customer(
-    val id: String,
+    val id: String?,
     val firstName: String,
     val lastName: String,
     val email: String
